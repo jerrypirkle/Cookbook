cf334528b5eb036748e0bc79fb3122a08d18fb1c
Jerry Pirkle <36513655+jerrypirkle@users.noreply.github.com>
2018-04-17 09:48:51 -0500

    Transposition cipher

--- Security/prezbo.py
+++ Security/prezbo.py
@@ -1,7 +1,20 @@
-#https://www.youtube.com/watch?v=DQBlq45c1T4
+#!/usr/bin/env python
+# # -*- coding: utf-8 -*-
+"""
+The Barksdale Code is a simple transposition cipher that was used in the TV show The Wire.
+Rules: 'Jump to the other side of the 5 on the keypad, and swap 5's and 0's.'
+See: https://www.youtube.com/watch?v=DQBlq45c1T4
+
+Author: Jerry Pirkle
+Date Created: 4/17/18
+Python Version: 3.6.4
+"""
+
 def decode(encodedNum):
-    """The Barksdale Code (Transposition Cipher) from The Wire:
-    Jump to the other side of the 5 on the keypad, and swap 5's and 0's."""
+    """
+    Input: unformatted phone number as a text string
+    Return: encoded / decoded plaintext string
+    """
     trans = {
     "1":"9", "2":"8", "3":"7",
     "4":"6", "5":"0", "6":"4",
