daeef60c1092221adf693b25869261c7e068857e
Jerry Pirkle <36513655+jerrypirkle@users.noreply.github.com>
2018-04-17 09:42:59 -0500

    Caesar cipher

--- Security/rot13.py
+++ Security/rot13.py
@@ -1,7 +1,9 @@
 #!/usr/bin/env python
 # # -*- coding: utf-8 -*-
 """
-Function that implements ROT13, a simple letter substitution cipher that replaces a letter with the 13th letter after it WITHOUT using built-in encode/decode from string module.
+Function that implements ROT13, a simple letter substitution cipher that replaces a 
+letter with the 13th letter after it WITHOUT using built-in encode/decode from string module. 
+This is a special case of the Caesar cipher which was developed in ancient Rome.
 Author: Jerry Pirkle
 Date Created: 3/25/18
 Python Version: 3.6.4
