240db169a707d29ad23d6d4c608f651bc9c6fa77
Jerry Pirkle <36513655+jerrypirkle@users.noreply.github.com>
2018-04-23 09:02:32 -0500

    Update objects.py

--- PythonReference/objects.py
+++ PythonReference/objects.py
@@ -1,19 +1,20 @@
+'''
 A blueprint for a house design is like a class description. All the houses built from that blueprint are objects of that class. A given house is an instance.
 
 A Dog is a class.
 An object is an instance of a specific dog. Your dog. Your neighbor's dog. Different dogs have different attribute values, but they all have the same attributes.
 Dogs have attributes, like a name. Attributes are what an instance IS.
 Dogs also have methods, like bork. Methods are what an instance can DO
+'''
 
-
-class Dog:							# Class (any) dog, will describe what it is (attribute) and what it can do (method)
+class Dog:				# Class (any) dog, will describe what it is (attribute) and what it can do (method)
 
     def __init__(self, name, age): 	# Initializer that used to instantiate objects. argument: self, refers to the object itself.
-        self.name = name			# Attribute associated with name argument ... what it is
+        self.name = name		# Attribute associated with name argument ... what it is
 		self.age = age
 
 		
-	def bork(self):					# Method ... what it does
+	def bork(self):			# Method ... what it does
 		print("ruff!")
 
 		
@@ -23,7 +24,7 @@ class Dog:							# Class (any) dog, will describe what it is (attribute) and wha
 	def birthday(self):
         self.age +=1
 		
-	 def setBuddy(self, bff):		# bff is a Dog object, object is reciprocal
+	 def setBuddy(self, bff):	# bff is a Dog object, object is reciprocal
         self.bff = bff
         bff.bff = self
 		
@@ -35,15 +36,15 @@ class Dog:							# Class (any) dog, will describe what it is (attribute) and wha
 >>> print(bailey.name + " is " + str(bailey.age) + " year(s) old.")
 [Out]: Bailey is 11 year(s) old.
 
->>> bailey.brrk()					# Methods can be called using dot notation
+>>> bailey.bork()			# Methods can be called using dot notation
 [Out]: ruff!
 
 >>> bailey.doginfo()
 [Out]: Bailey is 11 year(s) old.
 
->>> bailey.age = 12 				# Assign a new value to an attribute
+>>> bailey.age = 12 			# Assign a new value to an attribute
 		
->>> bailey.birthday()				# Can also call a method to reassign an attribute value
+>>> bailey.birthday()			# Can also call a method to reassign an attribute value
 >>> print(ozzy.age)
 [Out]: 13
 
